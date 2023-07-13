c160215b97e9dec3ddcf115f648f67cd6eb895c3
Edu <info.die@gmail.com>
2023-07-13 15:45:53 -0600

    update service

--- src/components/OurService.tsx
+++ src/components/OurService.tsx
@@ -9,7 +9,7 @@ const OurService = () => {
   return (
     <div className="h-[50vh] container mx-auto">
       <h1 className="text-5xl text-center my-8 font-bold">Our Service</h1>
-      <div className=" grid grid-cols-3 gap-5">
+      <div className=" grid grid-cols-1 md:grid-cols-3 px-2 gap-5">
         <Card
           icon={
             <BagCheck className="h-16 w-16 text-red-500 group-hover:text-orange-500 " />
