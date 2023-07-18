65801dcbd294fb89d2edfb791303588a273420c6
Edu <info.die@gmail.com>
2023-07-17 19:43:35 -0600

    new assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import OurService from "./components/OurService";
+import Popular from "./components/Popular/Popular";
 
 function App() {
   return (
@@ -8,6 +9,7 @@ function App() {
       <Navbar />
       <Hero />
       <OurService />
+      <Popular />
     </div>
   );
 }
