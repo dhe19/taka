8a696e00dbc90b66f171fa42d17cfb604555583d
Edu <info.die@gmail.com>
2023-07-17 18:49:34 -0600

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import OurService from "./components/OurService";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <div>
       <Navbar />
