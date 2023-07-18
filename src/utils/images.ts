e93dd64d90a2db680008467a0983e993222077ff
Edu <info.die@gmail.com>
2023-07-17 19:43:47 -0600

    add handle Images

---
+++ src/utils/images.ts
@@ -0,0 +1,6 @@
+import hamburger from '../assets/img/hamburger.jpg'
+import pizza from '../assets/img/pizza.jpg'
+import boneless from '../assets/img/boneless.jpg'
+import tacos from '../assets/img/tacos.jpg'
+
+export default { hamburger, pizza, boneless, tacos }
